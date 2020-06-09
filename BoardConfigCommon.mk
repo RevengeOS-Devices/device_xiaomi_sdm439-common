694d8ce80293a6e9d0ef772e32fb8183e1e11437
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-06-09 10:48:14 +0000

    sdm439-common: Import QC RIL
    * From daisy-user-10-QKQ1.191002.002-V11.0.5.0.QDLMIXM-release-keys
    
    Change-Id: I0281b82bf314641cc9c23e2c8b3f524fb0af185c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,6 +161,9 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.recovery.qcom
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
